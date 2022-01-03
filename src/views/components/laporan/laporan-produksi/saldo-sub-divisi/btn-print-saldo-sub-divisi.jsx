9efc7154305e2a0a1b3907f7a4752bfb40c40cf4
darisnugraha <50168058+darisnugraha@users.noreply.github.com>
2022-01-03 16:12:25 +0700

    update

--- src/views/components/laporan/laporan-produksi/saldo-sub-divisi/btn-print-saldo-sub-divisi.jsx
+++ src/views/components/laporan/laporan-produksi/saldo-sub-divisi/btn-print-saldo-sub-divisi.jsx
@@ -6,21 +6,32 @@ import "antd-button-color/dist/css/style.css";
 import ui from "../../../../../application/selectors/ui";
 import SaldoSubDivisi from "../../../../../application/selectors/saldosubdivisi";
 import ExcelReport from "./excel/excelReport";
+import ExcelReportHarian from "./excel/excelReportHarian";
 import pdfReport from "./pdf/pdfReport";
+import pdfReportHarian from "./pdf/pdfReportHarian";
 
 const BtnPrint = () => {
   // eslint-disable-next-line
   const btnLoading = useSelector(ui.getBtnLoading);
   const dataSaldoSubDivisi = useSelector(SaldoSubDivisi.getAllSaldoSubDivisi);
+  const type = useSelector(SaldoSubDivisi.getType);
 
   const pdfExportHandle = () => {
-    pdfReport(dataSaldoSubDivisi);
+    if (type === "ALL") {
+      pdfReport(dataSaldoSubDivisi);
+    } else {
+      pdfReportHarian(dataSaldoSubDivisi);
+    }
   };
 
   return (
     <Row style={{ marginTop: 15 }}>
       <Col span={10} style={{ marginTop: 10 }}>
-        <ExcelReport dataExel={dataSaldoSubDivisi} />
+        {type === "ALL" ? (
+          <ExcelReport dataExel={dataSaldoSubDivisi} />
+        ) : (
+          <ExcelReportHarian dataExel={dataSaldoSubDivisi} />
+        )}
       </Col>
       <Col htmltype="button" span={10} style={{ marginTop: 10 }} offset={2}>
         <Button
